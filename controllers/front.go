dbc838a6f8fdd9bfd6d62d44944d9d9c3453ebbb
Alireza <s.alireza.mohammadi@gmail.com>
2020-01-13 11:10:34 +0000

    Add functionality to front controller to encode responses as JSON

--- controllers/front.go
+++ controllers/front.go
@@ -2,9 +2,17 @@ package controllers
 
 import "net/http"
 
+import "encoding/json"
+
 func RegisterController() {
 	uc := newUserController()
 
 	http.Handle("/users", *uc)
 	http.Handle("/users/", *uc)
+}
+
+func encodeResponseAsJSON(data interface{}, w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application-json")
+	enc := json.NewEncoder(w)
+	_ = enc.Encode(data)
 }
